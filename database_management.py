c348f7b5d3e5ac05d6e879cd4d81f389db74589f
CosminACprojects <cosmin-catalin.farcas@student.tuiasi.ro>
2023-01-04 19:19:24 +0200

    creare inainte

--- database_management.py
+++ database_management.py
@@ -13,13 +13,81 @@ def create_tables_and_constraints():
     cursor=conn.cursor()
 
     cursor.execute("""
-        create table date_individ (
-            name varchar(25) not null
+        CREATE TABLE date_individ (
+            nr_telefon    CHAR(10) NOT NULL,
+            nume          VARCHAR(15) NOT NULL,
+            prenume       VARCHAR(25) NOT NULL,
+            data_nasterii DATE NOT NULL,
+            CHECK ( nr_telefon REGEXP '^[0][:7:3:2][0-9 ]*$' ),
+            CHECK ( LENGTH(nume) >= 2 AND nume REGEXP '^[a-zA-Z ]*$' ),
+            CHECK ( LENGTH(prenume) >= 2 AND prenume REGEXP '^[a-zA-Z ]*$' ),
+            PRIMARY KEY (nr_telefon)
         )
     """)
-    #haide
     cursor.execute("""
-        insert into date_individ values('Cosmin')
+        CREATE TABLE cont_client (
+            id_client     INT NOT NULL AUTO_INCREMENT,
+            rating        INT DEFAULT 10,
+            nr_inchirieri INT,
+            nr_telefon    CHAR(10) NOT NULL,
+            email         VARCHAR(25),
+            CHECK ( rating BETWEEN 0 AND 10 ),
+            CHECK ( email REGEXP '[a-z0-9._%-]+@[a-z0-9._%-]+\.[a-z]{2,4}' ),
+            UNIQUE INDEX cont_client__idx (nr_telefon),
+            PRIMARY KEY (id_client),
+            UNIQUE (email),
+            FOREIGN KEY (nr_telefon) REFERENCES date_individ (nr_telefon)
+        )
+    """)
+    cursor.execute("""
+       CREATE TABLE masina (
+            id_masina       INT NOT NULL AUTO_INCREMENT,
+            tip_masina      VARCHAR(15) NOT NULL,
+            an_fabricatie   DATE NOT NULL,
+            culoare         VARCHAR(15) NOT NULL,
+            pret_inchiriere INT NOT NULL,
+            PRIMARY KEY (id_masina),
+            CHECK (LENGTH(culoare) >= 2 AND culoare REGEXP '^[a-zA-Z ]*$'),
+            CHECK (pret_inchiriere > 0)
+        )
+    """)
+   
+    cursor.execute("""
+        CREATE TABLE cerere (
+            id_cerere    INT NOT NULL AUTO_INCREMENT,
+            data_inceput DATE NOT NULL,
+            data_retur   DATE NOT NULL,
+            id_client    INT NOT NULL,
+            id_masina    INT NOT NULL,
+            pret_total   INT NOT NULL,
+            PRIMARY KEY (id_cerere),
+            FOREIGN KEY (id_client) REFERENCES cont_client (id_client),
+            FOREIGN KEY (id_masina) REFERENCES masina (id_masina)
+        )
     """)
 
+    cursor.execute("""
+        CREATE TABLE lista_neagra (
+            motiv_suspendare VARCHAR(35) NOT NULL,
+            id_client        INT,
+            PRIMARY KEY (motiv_suspendare),
+            UNIQUE INDEX lista_neagra__idx (id_client),
+            FOREIGN KEY (id_client) REFERENCES cont_client (id_client)
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE status_masina (
+            id_masina        INT NOT NULL,
+            status           VARCHAR(15) NOT NULL,
+            stare_la_predare VARCHAR(20) DEFAULT 'BUNA',
+            stare_la_retur   VARCHAR(20),
+            data_retur_sm       DATE NOT NULL,
+            UNIQUE INDEX status_masina__idx (id_masina),
+            CHECK (status IN ('INCHIRIATA', 'RETURNATA')),
+            CHECK (stare_la_retur IN ('ACCIDENT', 'BUNA', 'ZGARIATA')),
+            FOREIGN KEY (id_masina) REFERENCES masina (id_masina) NOT DEFERRABLE
+        )
+    """)
+ 
     cursor.execute("commit")
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from database_management import *
 from flask import Flask,render_template,redirect
 
 
-app=Flask(__name__)
+#app=Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html',text='SUNT BOSS')
+#@app.route('/')
+#def home():
+#    return render_template('home.html',text='SUNT BOSS')
 
-
+create_tables_and_constraints()
